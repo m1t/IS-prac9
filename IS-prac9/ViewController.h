f7b9cd0d4189083d9170fbadf3e13bbc2c73322a
mits <mits.jp@gmail.com>
2013-12-15 14:15:19 +0900

    演習９完成

--- IS-prac9/ViewController.h
+++ IS-prac9/ViewController.h
@@ -10,4 +10,6 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)calc:(UIButton *)sender;
+
 @end
--- IS-prac9/ViewController.m
+++ IS-prac9/ViewController.m
@@ -10,6 +10,14 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *kg;
+@property (weak, nonatomic) IBOutlet UITextField *cm;
+@property (weak, nonatomic) IBOutlet UILabel *bmi;
+@property (weak, nonatomic) IBOutlet UIProgressView *prg;
+@property (weak, nonatomic) IBOutlet UILabel *msg;
+
+- (IBAction)bgTapped:(id)sender;
+
 @end
 
 @implementation ViewController
@@ -24,6 +32,53 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)bgTapped:(id)sender
+{
+    [self.view endEditing:YES];
+}
+
+- (IBAction)calc:(UIButton *)sender
+{
+    [self.view endEditing:YES];
+
+    float weight,height,bmi;
+    weight = [_kg.text floatValue];
+    height = [_cm.text floatValue] / 100;
+    bmi = [self calcBMI:weight tall:height];
+
+    _bmi.text = [NSString stringWithFormat:@"%.1f",bmi];
+    _prg.progress = (bmi -15) / 30;
+    
+    if (bmi<=0) {
+        _msg.text= @"体重と身長を入力してください";
+        _bmi.textColor = [UIColor blackColor];
+    } else if (bmi<18.5) {
+        _msg.text= @"低体重";
+        _bmi.textColor = [UIColor blueColor];
+    } else if (bmi<25.0) {
+        _msg.text= @"普通体重";
+        _bmi.textColor = [UIColor greenColor];
+    } else if (bmi<30.0) {
+        _msg.text= @"肥満（1度）";
+        _bmi.textColor = [UIColor yellowColor];
+    } else if (bmi<35.0) {
+        _msg.text= @"肥満（2度）";
+        _bmi.textColor = [UIColor orangeColor];
+    } else if (bmi<40.0) {
+        _msg.text= @"肥満（3度）";
+        _bmi.textColor = [UIColor purpleColor];
+    } else {
+        _msg.text= @"肥満（4度）";
+        _bmi.textColor = [UIColor redColor];
+    }
+}
+
+- (float)calcBMI:(float)kg tall:(float)m
+{
+    if ((kg <= 0) || (m <=0)) return 0;
+    return (kg / (m * m));
 }
 
 @end
